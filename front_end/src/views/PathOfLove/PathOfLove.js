3b2b779f01f90afc2f73bcca5d06238624e7d2fc
nguyen8amk1 <trungnguyen123akohshit@gmail.com>
2023-07-18 16:15:40 +0700

    add a little map properties panel on the left side

--- front_end/src/views/PathOfLove/PathOfLove.js
+++ front_end/src/views/PathOfLove/PathOfLove.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import './PathOfLove.scss'
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker, StandaloneSearchBox } from '@react-google-maps/api';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
-
 
 class PathOfLove extends React.Component {
     state = {
@@ -15,37 +13,64 @@ class PathOfLove extends React.Component {
             lat: 10.870272297526915, 
             lng: 106.80301118040023
         }, 
-
+        // Different zoom levels: 
+            // 1: World
+            // 5: Landmass/continent
+            // 10: City
+            // 15: Streets
+            // 20: Buildings
+        zoom: 15, 
         searchString: '',
         searchRecommendations: [
-            // {
-            //     name: 'thanh pho binh duong',
-            //     address: 'kong chro, khung long bao chua,...'
-            // },
-            // {
-            //     name: 'dat nuoc Gia Lai',
-            //     address: 'thi xa An Khe, khung long bao dam,...'
-            // },
-        ]
+        ],
+        markers: [
+            {
+                id: '345',
+                name: 'thanh pho binh duong',
+            },
+            {
+                id: '347',
+                name: 'dat nuoc Gia Lai',
+            },
+        ],
+        mapName: 'Test Map',
     }
 
     typingSearch(searchString) {
+        // TODO: doing some searches and display them into with the search recommendation
+
         this.setState({
             searchString: searchString,
             // @StartTest: test recommendation
             searchRecommendations: searchString.length > 0 ? [
                 {
+                    id: '123',
                     name: 'thanh pho binh duong',
                     address: 'kong chro, khung long bao chua,...'
                 },
                 {
+                    id: '124',
+                    name: 'dat nuoc Gia Lai',
+                    address: 'thi xa An Khe, khung long bao dam,...'
+                },
+                {
+                    id: '125',
+                    name: 'dat nuoc Gia Lai',
+                    address: 'thi xa An Khe, khung long bao dam,...'
+                },
+                {
+                    id: '126',
+                    name: 'dat nuoc Gia Lai',
+                    address: 'thi xa An Khe, khung long bao dam,...'
+                },
+                {
+                    id: '127',
                     name: 'dat nuoc Gia Lai',
                     address: 'thi xa An Khe, khung long bao dam,...'
                 },
             ]: []
             // @EndTest
         });
-        // TODO: doing some searches and display them into with the search recommendation
     }
 
     render() {
@@ -56,14 +81,30 @@ class PathOfLove extends React.Component {
                 <GoogleMap 
                     mapContainerStyle={this.state.containerStyle}
                     center={this.state.center}
-                    zoom={15}
+                    zoom={this.state.zoom}
                 >
                 <Marker position={this.state.center} />
                 </GoogleMap>
             </LoadScript>
 
+            {/* Map Properties */}
+            <div className='mapProperties'>
+                <div className='mapName'>
+                    {this.state.mapName}
+                </div>
+                <div className='mapMarkersInfo'>
+                    {
+                        this.state.markers.map((item, index) => {
+                            return (
+                                <div key={item.id} className='mapMarker'>{item.name}</div>
+                            );
+                        })
+                    }
+                </div>
+            </div>
+
             {/* <>SearchBar</> */}
-            <div className="wrap">
+            <div className="searchBarWrapper">
                 <div className="search">
                     <input type="text" className="searchTerm" value={this.state.searchString} onChange={event => this.typingSearch(event.target.value)}/>
                     <button type="submit" className="searchButton">
@@ -76,8 +117,8 @@ class PathOfLove extends React.Component {
                     {
                         this.state.searchRecommendations.map((item, index) => {
                             return (
-                                <div className='searchItem'>
-                                    <div>{item.name}</div>
+                                <div key={item.id} className='searchItem'>
+                                    <div>{item.name} - {item.address}</div>
                                 </div>
                             );
                         })
